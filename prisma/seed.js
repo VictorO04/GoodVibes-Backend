c0e220e3688c25184119fd6b9186d2e6ca6bb057
Murilow00 <milanmurilo7@gmail.com>
2025-11-03 15:42:29 -0300

    Feito o seed da tabela users

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,5 +3,24 @@ const prisma = new PrismaClient();
 
 
 async function main() {
-    
+    const user = [
+        { username: "Murilobmilan", email: "murilo.brustolin@gmail.com", password: "algumaCoisa", anonymous: true,  },
+        { username: "Victor", email: "victor.algo@gmail.com", password: "@senha123", anonymous: false,  }
+
+    ]
+
+    await prisma.users.createMany({data: user});
+
+    console.log('The seed worked very well 👍')
 }
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect(
+            process.exit(1)
+        )
+    })
