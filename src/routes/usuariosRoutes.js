a3f2de06554016ef5aef836164abbb87e4f858f1
Victor <victorfoliveira0407@gmail.com>
2025-11-28 15:46:30 -0300

    feat: update routes

--- src/routes/usuariosRoutes.js
+++ src/routes/usuariosRoutes.js
@@ -7,5 +7,6 @@ router.get("/", usuariosController.getAllUsuarios);
 router.get("/:id", usuariosController.getUsuariosById);
 router.post("/", usuariosController.createUsuario);
 router.delete("/:id", usuariosController.deleteUsuario);
+router.put("/:id", usuariosController.updateUsuario);
 
 export default router;
